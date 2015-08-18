a93fe56f853802da963ea07239545c13c6262c6b
Ekta <ektagoel04@gmail.com>
2015-08-19 02:29:30 +0530

    Generating 20 questions for the quiz, only from the topics selected by the user.

--- js/popup.js
+++ js/popup.js
@@ -23,21 +23,51 @@
                                 'options': options
                             });
                         });
-		});
-		}
-	});
-                var result = '<div><ul>';
-                $.each(questions, function(key, val) {
-                ++key;
-                result = result + '<li>';
-                result = result + key + ' ' + val.question + '<br>';                            
-                $.each(val.options, function(k, val) {
-                result = result + '<input name="' + key + '" type="radio">' + val + '<br>';
-                })
-                result = result + '</li>';
-                });
-                result = result + '</ul></div>';
-                $('body').html(result);
+					});
+				}
+			});
+			
+			array = [];
+			var size = 20;
+			
+			if ( questions.length < size)
+				size = questions.length;
+			
+			// quiz will have 20 questions at max
+			for(i=0;i<size;i++)
+				array[i] = i;
+			
+			// randomizing this array
+			var counter = array.length, temp, index;
+
+			// While there are elements in the array
+			while (counter > 0) {
+			// Pick a random index
+			index = Math.floor(Math.random() * counter);
+
+			// Decrease counter by 1
+			counter--;
+
+			// And swap the last element with it
+			temp = array[counter];
+			array[counter] = array[index];
+			array[index] = temp;
+			}
+			
+			//console.log(array);
+			
+			var result = '<div><ul>';
+			
+			for(i=0;i<size;i++){
+				result = result + '<li>';
+				//console.log(questions[array[i]].question);
+				result = result + (i+1) + '. ' + questions[array[i]].question + '<br>' ; 
+					for(var j =0; j< questions[array[i]].options.length ; j++){
+						result = result + '<input name = "' + (j+1) + '" type = "radio">' + questions[array[i]].options[j] + '<br>';
+					}
+				result = result + '<br>';
+			}
+            $('body').html(result);
 		});
         $('.checkbox_text').click(function() {
             var checked = $(this).prev().prop('checked');
@@ -49,7 +79,7 @@
             url: urlpassed,
             type: requestType,
             data: dynamicData,
-	    async: false
+			async: false
         });
     }
 }).call(this);
