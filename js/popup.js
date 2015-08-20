8468163dd3972cc5959289efcbdc5ab67adad57c
Ekta <ektagoel04@gmail.com>
2015-08-20 14:42:33 +0530

    Calculating score

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,19 @@
 (function() {
     $(document).ready(function(){
-        $('#submit').click(function(e){
+		function wrapper(id, cb) {
+			$(id).click(function(e){
+				cb(e);
+			});
+		}
+				
+        wrapper('#submit', function(e){
             e.preventDefault();
             var questions = [];
             var contentLeftToLoad = {
                 'totalSize': $('.checkboxCategory:checked').size(),
                 'loaded': 0
             };
+			
             $('.checkboxCategory:checked').each(function(){
                 var that = this;
                 ajax($(this).attr('data-url'), 'get').done(function(data){
@@ -16,23 +23,32 @@
                         val = $(val);
                         var question = val.children('.mtq_question_text').html();
                         var optionsHtml = val.find('.mtq_answer_text');
+						var ans = val.find('.mtq_marker.mtq_correct_marker');
+						var regexp = /mtq_marker-\d+-(\d+)-\d+/g
+						ans = $(ans).attr('id')
+						var match = regexp.exec(ans);
+						//console.log(match);
+						ans = match[1];
                         var options = [];
                         $.each(optionsHtml, function(key, val){
                             val = $(val);
                             options.push(val.html());
                         });
+						
                         questions.push({
                             'question': question,
-                            'options': options
+                            'options': options,
+							'answer': ans
                         });
                     });
                     ++contentLeftToLoad.loaded;
                 });
-            });
-            watch(contentLeftToLoad, 'loaded', function () {
-                if(contentLeftToLoad.loaded == contentLeftToLoad.totalSize) {
-                    allContentLoaded();
-                }
+				
+				watch(contentLeftToLoad, 'loaded', function () {
+					if(contentLeftToLoad.loaded == contentLeftToLoad.totalSize) {
+						allContentLoaded();
+					}
+				});
             });
 
             var allContentLoaded = function() {
@@ -71,17 +87,41 @@
                     //console.log(questions[array[i]].question);
                     result = result + (i+1) + '. ' + questions[array[i]].question + '<br>';
                     for(var j =0; j< questions[array[i]].options.length ; j++){
-                        result = result + '<input name = "' + (j+1) + '" type = "radio">' + questions[array[i]].options[j] + '<br>';
+                        result = result + '<input name = "' + (i) + '" type = "radio" value="' + (j+1) + '">' + questions[array[i]].options[j] + '<br>';
                     }
                     result = result + '<br>';
                 }
+				
+				result = result + "<p> Do you want to submit : </p>";
+				result  = result +'<button type = "Submit" name = "Submit" id = "submitanswers">Submit</button>';
+				
                 $('body').html(result);
+				
+				var score = [];
+				var s = 0;
+				
+				wrapper('#submitanswers', function(e) {
+					for(i = 0; i< size ; i++){
+						var ansSelected = $('input[name=' + i + ']:checked').attr('value');
+						score.push(ansSelected);
+						if ( ansSelected == questions[array[i]].answer){
+							s++;
+							console.log((i+1) + "Correct answer");
+						}
+						else{
+							console.log((i+1) + "Incorrect answer, the correct answer is " + questions[array[i]].answer);
+						}
+					}
+					console.log("Your score is " + s);
+				});
             };
         });
+		
         $('.checkbox_text').click(function() {
             var checked = $(this).prev().prop('checked');
             $(this).prev().prop('checked', !checked);
         });
+		
     });
     
     function ajax(urlpassed, requestType, dynamicData) {
